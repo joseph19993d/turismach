98272c774c0e48955f7c747cc808aca0c7d5a2a2
joseph19993d <rodelosuaresj@gmail.com>
2023-08-14 18:47:55 -0500

    home page, float button , welcome presentation, our services (don´t finish)

---
+++ src/components/textImage/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './style.css';
+
+const imageText =({text = 'any text'})=>{
+    <h1 className="centralTextName font-serif  ">
+        {text}
+    </h1>
+}
+
+export default imageText;
